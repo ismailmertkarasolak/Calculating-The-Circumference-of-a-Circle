2e97ad225ef3905093f6eb2a3fe087aebc43d190
Mert <115941460+ismailmertkarasolak@users.noreply.github.com>
2024-02-29 21:01:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ document.getElementById("mySUBMİT").onclick = function(){
     radius = document.getElementById("myTEXT").value;
     radius = Number(radius);
     circumference = 2 * pi * radius;
-    document.getElementById("myH3").innerHTML = "Çemberin Çevresi: " + circumference;
+    document.getElementById("myH3").innerHTML = "Circumferenc: " + circumference;
 }
